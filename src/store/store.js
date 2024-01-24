83b79dedb898dbc9b98be26783de02a2e5f94b32
harsh.sahni <harshsahni555@gmail.com>
2024-01-24 23:19:45 +0530

    fix: serializable check error fixed

--- src/store/store.js
+++ src/store/store.js
@@ -17,8 +17,11 @@ const rootReducer = combineReducers({
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 export const persistor = persistStore(store);
-
 
